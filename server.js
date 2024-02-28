c9f7278134c0229431588aa79688f225d14faa81
misprachi023 <prachimishra23123@gmail.com>
2024-02-28 10:36:04 +0530

    update

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')))
+// app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
 // app.get('*', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
